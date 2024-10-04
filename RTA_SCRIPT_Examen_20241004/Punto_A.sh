ba4af41f1cb850075f87b2a5ada462daba85242c
ValenDadamo <valentindadamo@gmail.com>
2024-10-04 18:39:28 -0300

    feat: Punto_A.sh , Crear la estructura de directorios para alumnos y profesores

---
+++ RTA_SCRIPT_Examen_20241004/Punto_A.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Crear la estructura de directorios
+mkdir -p /alumno_1/{parcial_1,parcial_2,parcial_3} /alumno_2/{parcial_1,parcial_2,parcial_3} /alumno_3/{parcial_1,parcial_2,parcial_3} /profesores
