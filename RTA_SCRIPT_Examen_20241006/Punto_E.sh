2e13f4fe5a2831302ff00fc000da0d5f178798c9
ValenDadamo <valentindadamo@gmail.com>
2024-10-13 03:18:30 +0000

    fix: Punto_E.sh

--- RTA_SCRIPT_Examen_20241006/Punto_E.sh
+++ RTA_SCRIPT_Examen_20241006/Punto_E.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-RUTA_BASE="$HOME/repogit/UTNFRA_SO_1P2C_2024_DAdamo/RTA_ARCHIVOS_Examen_20241006"
-
-echo "Filtrando la información del total de memoria RAM, pasandola al archivo 'Filtro_Basico.txt'."
-grep MemTotal /proc/meminfo > "$RUTA_BASE/Filtro_Basico.txt"
-
-echo "Agrego la información del fabricante del chasis."
-sudo dmidecode -t chassis | grep Chassis -A 1 >> "$RUTA_BASE/Filtro_Basico.txt"
-echo
-
-cat "$RUTA_BASE/Filtro_Basico.txt"
+grep MemTotal /proc/meminfo > ~/repogit/UTNFRA_SO_1P2C_2024_DAdamo/RTA_ARCHIVOS_Examen_20241006/Filtro_Basico.txt
+sudo dmidecode -t chassis | grep Manufacturer >> Filtro_Basico.txt
+echo "Fin Punto E"
 
