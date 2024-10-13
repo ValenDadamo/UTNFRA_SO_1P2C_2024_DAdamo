2810d3c3f6e07a10ec43f477949c2d48b173eb2e
ValenDadamo <valentindadamo@gmail.com>
2024-10-13 02:53:53 +0000

    fix: Punto_C.sh

--- RTA_SCRIPT_Examen_20241006/Punto_C.sh
+++ RTA_SCRIPT_Examen_20241006/Punto_C.sh
@@ -1,46 +1,52 @@
 #!/bin/bash
 
-echo "Creo grupos secundarios."
+# Crear grupos
+echo "Creando grupos secundarios..."
 sudo groupadd p1c2_2024_gAlumno
 sudo groupadd p1c2_2024_gProfesores
 echo
 
-echo "Cambio la clave del usuario vagrant."
-echo "vagrant:clave1" | sudo chpasswd
+# Obtener el hash de la contraseña del usuario actual
+PASSWD="$(sudo grep $(whoami) /etc/shadow | awk -F ':' '{ print $2 }')"
+
+# Crear usuarios con el hash de la contraseña
+echo "Creando usuarios..."
+sudo useradd -m -s /bin/bash -G p1c2_2024_gAlumno -p "$PASSWD" p1c2_2024_A1
+sudo useradd -m -s /bin/bash -G p1c2_2024_gAlumno -p "$PASSWD" p1c2_2024_A2
+sudo useradd -m -s /bin/bash -G p1c2_2024_gAlumno -p "$PASSWD" p1c2_2024_A3
+sudo useradd -m -s /bin/bash -G p1c2_2024_gProfesores -p "$PASSWD" p1c2_2024_P1
 echo
 
-echo "Creo todos los usuarios con la misma clave que vagrant."
-for i in {1..3}; do
-    sudo useradd -m -s /bin/bash -G p1c2_2024_gAlumno -p "$(sudo grep vagrant /etc/shadow | awk -F ':' '{print $2}')" p1c2_2024_A$i
-done
-sudo useradd -m -s /bin/bash -G p1c2_2024_gProfesores -p "$(sudo grep vagrant /etc/shadow | awk -F ':' '{print $2}')" p1c2_2024_P1
+# Ajustar propietarios y permisos de las carpetas
+echo "Ajustando propietarios y permisos..."
+sudo chown -R p1c2_2024_A1:p1c2_2024_gAlumno /Examenes-UTN/alumno_1
+sudo chmod 750 /Examenes-UTN/alumno_1
+
+sudo chown -R p1c2_2024_A2:p1c2_2024_gAlumno /Examenes-UTN/alumno_2
+sudo chmod 760 /Examenes-UTN/alumno_2
+
+sudo chown -R p1c2_2024_A3:p1c2_2024_gAlumno /Examenes-UTN/alumno_3
+sudo chmod 700 /Examenes-UTN/alumno_3
+
+sudo chown -R p1c2_2024_P1:p1c2_2024_gProfesores /Examenes-UTN/profesores
+sudo chmod 770 /Examenes-UTN/profesores
 echo
 
-echo "Cambio los propietarios y grupos a las carpetas creadas en el Punto_A."
-for i in {1..3}; do
-    sudo chown -R p1c2_2024_A$i:p1c2_2024_gAlumno ~/Examenes-UTN/alumno_$i/
-done
-sudo chown -R p1c2_2024_P1:p1c2_2024_gProfesores ~/Examenes-UTN/profesores/
+# Crear archivo validar.txt
+echo "Creando archivo validar.txt..."
+sudo -u p1c2_2024_A1 bash -c "whoami > /Examenes-UTN/alumno_1/validar.txt"
+sudo -u p1c2_2024_A2 bash -c "whoami > /Examenes-UTN/alumno_2/validar.txt"
+sudo -u p1c2_2024_A3 bash -c "whoami > /Examenes-UTN/alumno_3/validar.txt"
+sudo -u p1c2_2024_P1 bash -c "whoami > /Examenes-UTN/profesores/validar.txt"
 echo
 
-echo "Cambio los permisos de cada carpeta."
-sudo chmod 750 ~/Examenes-UTN/alumno_1/
-sudo chmod 760 ~/Examenes-UTN/alumno_2/
-sudo chmod 700 ~/Examenes-UTN/alumno_3/
-sudo chmod 777 ~/
+# Mostrar contenido de validar.txt
+echo "Contenido de los archivos validar.txt:"
+sudo -u p1c2_2024_A1 bash -c "cat /Examenes-UTN/alumno_1/validar.txt"
+sudo -u p1c2_2024_A2 bash -c "cat /Examenes-UTN/alumno_2/validar.txt"
+sudo -u p1c2_2024_A3 bash -c "cat /Examenes-UTN/alumno_3/validar.txt"
+sudo -u p1c2_2024_P1 bash -c "cat /Examenes-UTN/profesores/validar.txt"
 echo
 
-echo "Creo el archivo 'validar.txt' con el nombre de cada usuario en sus respectivas carpetas."
-for i in {1..3}; do
-    sudo -u p1c2_2024_A$i bash -c "whoami > /home/vagrant/Examenes-UTN/alumno_$i/validar.txt"
-done
-sudo -u p1c2_2024_P1 bash -c "whoami > /home/vagrant/Examenes-UTN/profesores/validar.txt"
-echo
-
-echo "Muestro el contenido de los archivos 'validar.txt'."
-for i in {1..3}; do
-    sudo -u  p1c2_2024_A$i bash -c "cat /home/vagrant/Examenes-UTN/alumno_$i/validar.txt"
-done
-sudo -u p1c2_2024_P1 bash -c "cat /home/vagrant/Examenes-UTN/profesores/validar.txt"
-echo
+echo "Script finalizado."
 
