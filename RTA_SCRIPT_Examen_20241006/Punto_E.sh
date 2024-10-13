793f218970a31220ee9b386e33c4f731557da1f9
ValenDadamo <valentindadamo@gmail.com>
2024-10-13 03:54:15 +0000

    fix: Punto_E.sh  |  Punto_F.sh

--- RTA_SCRIPT_Examen_20241006/Punto_E.sh
+++ RTA_SCRIPT_Examen_20241006/Punto_E.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 grep MemTotal /proc/meminfo > ~/repogit/UTNFRA_SO_1P2C_2024_DAdamo/RTA_ARCHIVOS_Examen_20241006/Filtro_Basico.txt
-sudo dmidecode -t chassis | grep Manufacturer >> Filtro_Basico.txt
+sudo dmidecode -t chassis | grep Manufacturer >> ~/repogit/UTNFRA_SO_1P2C_2024_DAdamo/RTA_ARCHIVOS_Examen_20241006/Filtro_Basico.txt
 echo "Fin Punto E"
 
--- RTA_SCRIPT_Examen_20241006/Punto_F.sh
+++ RTA_SCRIPT_Examen_20241006/Punto_F.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-echo "Nos dirigimos al repositorio."
-cd $HOME/repogit/UTNFRA_SO_1P2C_2024_DAdamo
-
-echo "Filtrando todas las líneas, enviándolas al archivo 'Filtro_Avanzado.txt'."
-echo -e "Mi IP Pública es: $(curl -s ifconfig.me | grep -v vagrant)\nMi usuario es: $(whoami)\nEl Hash de mi Usuario es: $(sudo grep vagrant /etc/shadow | awk -F ':' '{print $2}')\nLa URL de mi repositorio es: $(git remote get-url origin)" > RTA_ARCHIVOS_Examen_20241006/Filtro_Avanzado.txt
-
-echo "Leo el archivo 'Filtro_Avanzado.txt'."
-cat RTA_ARCHIVOS_Examen_20241006/Filtro_Avanzado.txt
+echo "Mi IP Publica es: $(curl -s ifconfig.me)" > ~/repogit/UTNFRA_SO_1P2C_2024_DAdamo/RTA_ARCHIVOS_Examen_20241006/Filtro_Avanzado.txt
+echo "Mi usuario es: $(whoami)" >> ~/repogit/UTNFRA_SO_1P2C_2024_DAdamo/RTA_ARCHIVOS_Examen_20241006/Filtro_Avanzado.txt
+echo "El Hash de mi usuario es: $(sudo grep $(whoami) /etc/shadow | awk -F ':' '{print $2}')" >> ~/repogit/UTNFRA_SO_1P2C_2024_DAdamo/RTA_ARCHIVOS_Examen_20241006/Filtro_Avanzado.txt
+echo "La URL de mi repositorio es: $(git remote get-url origin)" >> ~/repogit/UTNFRA_SO_1P2C_2024_DAdamo/RTA_ARCHIVOS_Examen_20241006/Filtro_Avanzado.txt
+echo "Fin Punto F"
 
