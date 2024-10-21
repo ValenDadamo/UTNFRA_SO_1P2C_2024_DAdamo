96f9b2f39fbd064458f290e2618fdd5b77133a57
ValenDadamo <valentindadamo@gmail.com>
2024-10-21 03:23:32 +0000

    fix check_D.sh OK

--- RTA_SCRIPT_Examen_20241006/Punto_D.sh
+++ RTA_SCRIPT_Examen_20241006/Punto_D.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir -p ~/Estructura_Asimetrica/{correo/{cartas_{1..100},carteros_{1..10}},clientes/cartas_{1..100}}
+mkdir -p ~/Estructura_Asimetrica/{correo/{cartas_{1..100},carteros_{1..10}},cliente/cartas_{1..100}}
 
 tree ~/Estructura_Asimetrica/ --noreport | pr -T -s' ' -w 80 --column 4
 
